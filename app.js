24e99c2b93b05518e05fa36fc968e93f4c38e27c
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-02-26 14:39:39 +0100

    Add: Cors

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express"); //Richiama l'utilizzo di Express
+const cors = require("cors")
+
 const app = express(); //Definisce Express nella costante app
 const port = 3000; //Definisce una porta alla quale assegnare i comandi
 const checkTime = require("./middlewares/checkTime")
 const errorsHandler = require("./middlewares/errorsHandler")
+const notFound = require("./middlewares/notFound")
 
 const postsRouter = require("./routers/postRouter"); //Richiama il routers
+app.use(cors({
+  // origin: "http://localhost:5173"
+}));
+
 
 app.use(checkTime);
-app.use(errorsHandler);
+
 
 app.use(express.static("public")); //Consente l'accesso alla cartella pubblic
 
@@ -19,6 +26,9 @@ app.use("/posts", postsRouter);
 app.get("/", (req, res) => {
   res.send(`Server della mia pizzeria`);
 });
+
+app.use(errorsHandler);
+app.use (notFound);
 
 //attivazione del server
 app.listen(port, () => {
